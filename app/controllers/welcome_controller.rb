0c7c9466e0647cdf525d31a38bb6c1abfc6af561
Chris Simmons <chris@turing.edu>
2024-02-15 10:18:23 -0500

    Adds contact page and details

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,6 @@ class WelcomeController < ApplicationController
   def index; end
 
   def about; end
+
+  def contact; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :projects, only: [:index, :show]
   get "/about", to: "welcome#about"
+  get "/contact", to: "welcome#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

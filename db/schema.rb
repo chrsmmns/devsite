224ccc7e65ddee6c061f0b0858198a936ff35a3d
Chris Simmons <chris@turing.edu>
2024-02-15 10:08:59 -0500

    Updates rails versions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_07_211345) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_15_150648) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "short_desc"
